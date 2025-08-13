faaff8d5f7ee020f18043a84f6968a661c2e4e5a
s4m-0206 <arbecast@gmail.com>
2025-08-13 14:23:22 -0500

    Add files via upload

---
+++ 3.Condicionales/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*1 Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona,
+                 si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla 
+                 con el nombre de la persona,
+                 indicando que debe abonar impuestos.*/
+
+            //condicional simple
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el Nombre");
+            nombre = Console.ReadLine();//todo lo que yo leo desde la consola llega como un string 
+
+
+            Console.WriteLine(" Ingrese sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000)
+            {
+                Console.WriteLine($"la persona {nombre}, debe abonar impuestos");
+            }
+
+            /*Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18,
+             * se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”.*/
+
+
+
+        }
+    }
+}
