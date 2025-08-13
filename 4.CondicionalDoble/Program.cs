e7433e7fa677c62c1b0c03ffcfbc755a818cbc41
s4m-0206 <arbecast@gmail.com>
2025-08-13 15:26:50 -0500

    Add files via upload

---
+++ 4.CondicionalDoble/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _4.CondicionalDoble
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("Ingrese el nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el sueldo");
+            sueldo = Int32.Parse(Console.ReadLine());
+
+            if (sueldo >= 3000)
+            {
+                //verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Falsa
+                Console.WriteLine($"La persona {nombre}, no abona impuestos");
+            }
+            
+
+        }       
+
+        
+    }
+}
