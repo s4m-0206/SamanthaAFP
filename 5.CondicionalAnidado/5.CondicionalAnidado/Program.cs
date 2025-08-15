e8ae1eb5644fe0e132589c6bdcc8cef00ae05179
s4m-0206 <arbecast@gmail.com>
2025-08-15 15:31:26 -0500

    Add files via upload

---
+++ 5.CondicionalAnidado/5.CondicionalAnidado/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _5.CondicionalAnidado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONDICIONAL ANIDADO
+            /*.Diseñar un algoritmo que lea tres numeros A, B, C y visualice en 
+             * pantalla el valor mas grande. El usuario debe ingresar tres valores 
+             * diferentes.*/
+            int num1 = 0;
+            int num2 = 0;   
+            int num3 = 0;
+
+            Console.WriteLine("Ingrese el numero 1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2:");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero3:");
+            num3 = Int32.Parse(Console.ReadLine());
+
+            if(num1>num2)
+            {
+                if(num2>num3)
+                {
+                    Console.WriteLine($"Numero 1 : {num1}, es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine($"Numero 3 : {num3}, es el mayor");
+                }
+            }
+            else
+            {
+                if (num2>num3)
+                {
+                    Console.WriteLine($"Numero 2 : {num2}, es el mayor");
+                }
+                else
+                {
+                    Console.WriteLine($"Numero 3 : {num3}, es el mayor");
+                }
+            }
+        }
+    }
+}
