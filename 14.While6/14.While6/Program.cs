b45419a97bac024969eefe8489041fb5f97442cf
s4m-0206 <arbecast@gmail.com>
2025-09-05 14:18:22 -0500

    Add files via upload

---
+++ 14.While6/14.While6/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _14.While6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerosazar = 0;
+            int cantidadnumeros = 0;
+            int contador = 0;
+            int mayorcero = 0;
+            int menorcero = 0;
+            int igualcero = 0;
+
+            Console.WriteLine("cuantos numeros quiere ingresar");
+            cantidadnumeros = int.Parse(Console.ReadLine());
+
+            while (contador <= cantidadnumeros)
+            {
+                Console.WriteLine($"numeros al azar #{contador + 1}");
+                numerosazar = int.Parse(Console.ReadLine());
+
+                if (numerosazar > 0)
+                {
+                    mayorcero++;
+                }
+                else if (numerosazar < 0)
+                {
+                    menorcero++;
+
+                }
+                if (numerosazar == 0)
+                {
+                    igualcero++;
+                }
+                contador++;
+
+            }
+            Console.WriteLine($"numeros mayores {mayorcero}");
+            Console.WriteLine($"numeros menor {menorcero}");
+            Console.WriteLine($"numeros igual {igualcero}");
+
+        }
+    }
+}
