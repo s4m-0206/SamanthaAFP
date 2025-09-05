fa73f72ff4c12e43dc9503f20b5ff3101a31aba1
s4m-0206 <arbecast@gmail.com>
2025-09-05 14:10:22 -0500

    Add files via upload

---
+++ 9.CicloWhile/9.CicloWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un algoritmo que permita generar primeros 5 numeros enteros positivos y realizar y mostrar su suma.
+
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador; //acumulador =acumulador+contador
+                contador++;
+            }
+            
+            Console.WriteLine($"La suma de los cinco primeros numeros enteros es: {acumulador}");
+        }
+    }
+}
++
