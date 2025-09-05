f60eeb168e589bd9a07f931669a6b4c2ec6366ca
s4m-0206 <arbecast@gmail.com>
2025-09-05 14:10:48 -0500

    Add files via upload

---
+++ 10.While2/10.While2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un numero dado por el usuario(5! = 1*2*3*4*5)
+            int num = 0;
+            int contador = 1;
+            int acumulador = 0;
+            Console.WriteLine("Ingrese un numero para calcular su factorial");
+            num = int.Parse(Console.ReadLine());
+
+            while (contador<=num)
+            {
+                acumulador = acumulador * contador;
+                Console.WriteLine($"Contador: {contador} - Factorial: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"El factorial es numero {num}, es: {acumulador}");
+        }
+    }
+}
