779d1768e4b0c3f2a880a62ed708e02d2362adc7
s4m-0206 <arbecast@gmail.com>
2025-09-17 15:11:03 -0500

    Add files via upload

---
+++ 21.Ejercicio/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _21.Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                acumulador += i;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros números enteros es {acumulador}");
+        }
+    }
+}
