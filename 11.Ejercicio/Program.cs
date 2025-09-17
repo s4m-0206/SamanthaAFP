3c4123dbebd9750c09245643ec2258e759741a1a
s4m-0206 <arbecast@gmail.com>
2025-09-17 14:39:35 -0500

    Add files via upload

---
+++ 11.Ejercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _11.Ejercicio
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int numero;
+            int suma = 0;
+
+            Console.WriteLine("Ingrese números enteros positivos para sumarlos.");
+            Console.WriteLine("Ingrese un número negativo para finalizar.");
+
+            do
+            {
+                Console.Write("Número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero >= 0)
+                {
+                    suma += numero;
+                }
+
+            } while (numero >= 0);
+
+            Console.WriteLine($"La suma de los números ingresados es: {suma}");
+        }
+    }
+}
