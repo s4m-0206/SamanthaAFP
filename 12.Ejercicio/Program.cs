443cc027e3e7eee3122c9950a4f37b3952ce716e
s4m-0206 <arbecast@gmail.com>
2025-09-17 14:43:51 -0500

    Add files via upload

---
+++ 12.Ejercicio/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _12.Ejercicio
+{
+    class Program
+    {
+        static void Main()
+        {
+            int suma = 0;
+
+            for (int i = 1; i <= 99; i += 2) // Recorremos solo los impares
+            {
+                suma += i;
+            }
+
+            Console.WriteLine($"La suma de los números impares hasta 99 es: {suma}");
+        }
+    }
+}
