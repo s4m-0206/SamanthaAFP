cdf9fd62fb23830eabe4b1aa518727027bc64634
s4m-0206 <arbecast@gmail.com>
2025-10-03 14:19:31 -0500

    Add files via upload

---
+++ 24.Ejercicio/24.Ejercicio/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _24.Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeros = new double[10];
+            double suma = 0;
+
+            // Leer los 10 números
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Ingrese el número " + (i + 1) + ": ");
+                numeros[i] = Convert.ToDouble(Console.ReadLine());
+                suma += numeros[i];
+            }
+
+            // Calcular el promedio
+            double promedio = suma / numeros.Length;
+
+            // Mostrar el promedio
+            Console.WriteLine("\nEl promedio de los números ingresados es: " + promedio);
+        }
+    }
+}
---
+++ 26.Ejercicio/26.Ejercicio/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _26.Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int filas = 2, columnas = 3;
+                int[,] matriz1 = new int[filas, columnas];
+                int[,] matriz2 = new int[filas, columnas];
+                int[,] suma = new int[filas, columnas];
+
+                Console.WriteLine("Ingrese los elementos de la primera matriz (2x3):");
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        Console.Write($"Matriz1[{i},{j}]: ");
+                        matriz1[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+
+                Console.WriteLine("\nIngrese los elementos de la segunda matriz (2x3):");
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        Console.Write($"Matriz2[{i},{j}]: ");
+                        matriz2[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+
+                // Calcular la suma de las matrices
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        suma[i, j] = matriz1[i, j] + matriz2[i, j];
+                    }
+                }
+
+                // Mostrar matriz resultante
+                Console.WriteLine("\nLa matriz suma es:");
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < columnas; j++)
+                    {
+                        Console.Write(suma[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
