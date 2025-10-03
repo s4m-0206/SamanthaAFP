4ba0f46f9158d294e7bc8ff4ad92cc4946e2031c
s4m-0206 <arbecast@gmail.com>
2025-10-03 14:14:44 -0500

    Add files via upload

---
+++ 23.Ejercicio/23.Ejercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _23.Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[100];
+
+            // Asignar el valor 10 a cada posición
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 10;
+            }
+
+            // Leer y mostrar cada elemento
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("numeros[" + i + "] = " + numeros[i]);
+            }
+        }
+    }
+}
